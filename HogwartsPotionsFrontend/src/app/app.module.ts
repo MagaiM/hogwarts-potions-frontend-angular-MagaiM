04d7fab47903207ec3d12a0d6acc9c4e76d7de3b
MagaiM <magai.marton@gmail.com>
2022-10-13 03:48:11 +0200

    Add declarations to new components

--- HogwartsPotionsFrontend/src/app/app.module.ts
+++ HogwartsPotionsFrontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { PotionsComponent } from './potions/potions.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    PotionsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
