92fa6c05719a5362d5d3a5e8bbc782aed3f9b1d4
MagaiM <magai.marton@gmail.com>
2022-10-27 10:23:32 +0200

    Add room-by-id component

---
+++ HogwartsPotionsFrontend/src/app/room-by-id/room-by-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-by-id',
+  templateUrl: './room-by-id.component.html',
+  styleUrls: ['./room-by-id.component.css']
+})
+export class RoomByIdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
