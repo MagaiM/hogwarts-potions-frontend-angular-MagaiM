736ae10a0f04087d16de76cca076df38ef7c3eb4
MagaiM <magai.marton@gmail.com>
2022-10-28 00:39:10 +0200

    Add add and goToNewRoom methods to create-room.components.ts

--- HogwartsPotionsFrontend/src/app/create-room/create-room.component.ts
+++ HogwartsPotionsFrontend/src/app/create-room/create-room.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {  Router } from '@angular/router';
+
+import { Room } from '../room';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-create-room',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-room.component.css']
 })
 export class CreateRoomComponent implements OnInit {
+  room: Room = {
+    capacity : 5,
+    residents:[],
+    id: 0
+  };
 
-  constructor() { }
+  capacity: Number = 5;
+  
+  constructor(
+    private roomService: RoomService,
+    public router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  goToNewRoom(): void {
+    this.router.navigate([`rooms/details/${this.room.id}`]);
+  }
+
+  add(capacity: string): void {
+
+    if (!capacity) {
+      return;
+    }
+    this.room.capacity = Number(capacity);
+    this.roomService.addRoom(this.room).subscribe((room) => {
+      this.room = room;
+      this.goToNewRoom();
+    });
+  }
 }
