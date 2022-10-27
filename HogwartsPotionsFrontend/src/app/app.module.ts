71f2176863cf01c8ba5eb2aee1921be4ab3a6a92
MagaiM <magai.marton@gmail.com>
2022-10-27 10:25:20 +0200

    Add new components to declarations, Add httpClientModule to imports

--- HogwartsPotionsFrontend/src/app/app.module.ts
+++ HogwartsPotionsFrontend/src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { PotionsComponent } from './potions/potions.component';
 import { HomeComponent } from './home/home.component';
+import { AllRoomsComponent } from './all-rooms/all-rooms.component';
+import { RoomByIdComponent } from './room-by-id/room-by-id.component';
+import { RatFriendlyRoomsComponent } from './rat-friendly-rooms/rat-friendly-rooms.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
+import { DeleteRoomComponent } from './delete-room/delete-room.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoomsComponent,
     PotionsComponent,
-    HomeComponent
+    HomeComponent,
+    AllRoomsComponent,
+    RoomByIdComponent,
+    RatFriendlyRoomsComponent,
+    CreateRoomComponent,
+    UpdateRoomComponent,
+    DeleteRoomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
