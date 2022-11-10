834230d2681ef33e1b2469ea74890045496fb0ba
MagaiM <magai.marton@gmail.com>
2022-11-10 23:50:18 +0100

    Add new student routes

--- HogwartsPotionsFrontend/src/app/app-routing.module.ts
+++ HogwartsPotionsFrontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllRoomsComponent } from './all-rooms/all-rooms.component';
+import { AllStudentsComponent } from './all-students/all-students.component';
 import { CreateRoomComponent } from './create-room/create-room.component';
 import { HomeComponent } from './home/home.component';
 import { PotionsComponent } from './potions/potions.component';
@@ -8,6 +9,8 @@ import { RatFriendlyRoomsComponent } from './rat-friendly-rooms/rat-friendly-roo
 import { RoomByIdComponent } from './room-by-id/room-by-id.component';
 import { RoomDetailComponent } from './room-detail/room-detail.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { StudentsComponent } from './students/students.component';
 
 const routes: Routes = [
   {path: '', redirectTo: "home", pathMatch: 'full' },
@@ -38,6 +41,17 @@ const routes: Routes = [
       }
     ]
   },
+  {path: 'students', component: StudentsComponent,
+    children: [
+      {
+        path: 'all-students',
+        component: AllStudentsComponent
+      },
+      {
+        path: 'details/:id',
+        component: StudentDetailComponent,
+      }
+    ]},
   {path: 'potions', component: PotionsComponent },
 
   {path: '**', redirectTo: "home", pathMatch: 'full' },
