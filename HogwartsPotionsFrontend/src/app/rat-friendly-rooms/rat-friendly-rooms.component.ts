67dfb9a091d55c298dfe6043e4b3a6040aa89993
MagaiM <magai.marton@gmail.com>
2022-11-10 21:17:54 +0100

    Add getRooms, delete methods to rat-friendly-rooms.componet.ts

--- HogwartsPotionsFrontend/src/app/rat-friendly-rooms/rat-friendly-rooms.component.ts
+++ HogwartsPotionsFrontend/src/app/rat-friendly-rooms/rat-friendly-rooms.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Room } from '../room';
+import { RoomService } from '../room.service';
+
 @Component({
   selector: 'app-rat-friendly-rooms',
   templateUrl: './rat-friendly-rooms.component.html',
   styleUrls: ['./rat-friendly-rooms.component.css']
 })
 export class RatFriendlyRoomsComponent implements OnInit {
+  rooms: Room[] = [];
+  houseTypes: String[] = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
-  constructor() { }
+  constructor(private roomService: RoomService) { }
+
 
   ngOnInit(): void {
+    this.getRooms();
   }
 
+  getRooms(): void {
+    this.roomService.getRatFriendlyRooms()
+    .subscribe(rooms => {
+      this.rooms = rooms;
+    });
+  }
+
+  delete(room: Room): void {
+    this.rooms = this.rooms.filter(r => r !== room);
+    this.roomService.deleteRoom(room.id).subscribe();
+  }
 }
