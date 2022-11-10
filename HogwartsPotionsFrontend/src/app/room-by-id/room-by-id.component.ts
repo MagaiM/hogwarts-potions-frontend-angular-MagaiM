cce333e0572fe62fe11d437da0f1f955ec4c9425
MagaiM <magai.marton@gmail.com>
2022-11-10 20:34:57 +0100

    Add isChange, goToRoom, search methods to room-by-id.component.ts, Add properties to room-by-id.component.ts

--- HogwartsPotionsFrontend/src/app/room-by-id/room-by-id.component.ts
+++ HogwartsPotionsFrontend/src/app/room-by-id/room-by-id.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {  Router } from '@angular/router';
+
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-room-by-id',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-by-id.component.css']
 })
 export class RoomByIdComponent implements OnInit {
+  roomNumber: number | undefined;
+  validNumberToSearch: boolean = false;
+  errorMessage: string | undefined;
 
-  constructor() { }
+
+  constructor(
+    private roomService: RoomService,
+    public router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  isChanged(): void {
+    if (this.roomNumber) {
+      if (this.roomNumber > 0) {
+        this.validNumberToSearch = true;
+        return;
+      }
+    }
+    this.validNumberToSearch = false;
+  }
+
+  goToRoom(): void {
+    this.router.navigate([`/rooms/details/${this.roomNumber}`]);
+  }
+
+  search(): void {
+    if (this.roomNumber) {
+      const id = this.roomNumber;
+      this.roomService.getRoom(this.roomNumber)
+      .subscribe(room => {
+        if (!room) {
+          this.errorMessage = `Room with this number: ${id} does not exists!`;
+          return;
+        }
+        this.goToRoom();
+      });
+    }
+  }
 }
